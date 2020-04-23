f518075807c6b5808c09ae02ea7790a46a95f9ac
Michael <60555279+msheets1983@users.noreply.github.com>
2020-04-23 17:05:57 -0500

    more testing

--- user-onboarding/cypress/integration/examples/form_test.spec.js
+++ user-onboarding/cypress/integration/examples/form_test.spec.js
@@ -3,7 +3,11 @@ it('works!', () => {
     expect(6).to.eq(6)
 })
 
-describe('Friends Form', () => {
+const userName = 'Michael'
+const userEmail = "michael.sheets83@gmail.com"
+const password = 'Password1'
+
+describe('User Onboarding', () => {
     it('can navigate to the site', () => {
         cy.visit('')
         cy.url().should('include', 'localhost')
@@ -11,20 +15,20 @@ describe('Friends Form', () => {
 
     it('Can input NAME, type NAME in it, and assertion.', () => {
         cy.get('input[name="name"]')
-            .type('Michael')
-            .should('have.value', 'Michael')
+            .type(userName)
+            .should('have.value', userName)
     })
 
     it('Can input EMAIL and type EMAIL in it, and assertion.', () => {
         cy.get('input[name="email"]')
-            .type('Michael.sheets83@gmail.com')
-            .should('have.value', 'Michael.sheets83@gmail.com')
+            .type(userEmail)
+            .should('have.value', userEmail)
     })
 
     it('Can input PASSWORD and type PASSWORD in it, and assertion.', () => {
         cy.get('input[name="userPassword"]')
-            .type('password')
-            .should('have.value', 'password')
+            .type(password)
+            .should('have.value', password)
     })
 
     it('Can check TERMS OF SERVICE, and assertion.', () => {
@@ -36,35 +40,105 @@ describe('Friends Form', () => {
         cy.contains('Submit')
             .click()
 
-            cy.wait(500)
+        cy.wait(500)
     })
 
 
-    it('has validation error if NAME < 3 chars', () =>{
+    it('Has validation error if NAME < 3 chars', () => {
         cy.get('input[name="name"]')
-        .type('a')
-        .should('have.value', 'a')
+            .type('a')
+            .should('have.value', 'a')
 
         cy.get('.error')
-        .contains('this must be at least 3 characters')
+            .contains('this must be at least 3 characters')
 
         cy.wait(500)
 
         cy.get('input[name="name"]')
-        .type('b')
-        .should('have.value', 'ab')
+            .type('b')
+            .should('have.value', 'ab')
 
         cy.get('.error')
-        .contains('this must be at least 3 characters')
+            .contains('this must be at least 3 characters')
 
         cy.wait(500)
 
         cy.get('input[name="name"]')
-        .type('c')
-        .should('have.value', 'abc')
+            .type('c')
+            .should('have.value', 'abc')
 
         cy.contains('this must be at least 3 characters')
-        .should('not.exist')
+            .should('not.exist')
     })
 
+    it('Has validation error if EMAIL is not correct format', () => {
+        cy.get('input[name="email"]')
+            .type('password')
+            .should('have.value', 'password')
+
+        cy.get('.error')
+            .contains('this must be a valid email')
+
+        cy.wait(500)
+
+        cy.get('input[name="email"]')
+            .type('@')
+            .should('have.value', 'password@')
+
+        cy.get('.error')
+            .contains('this must be a valid email')
+
+        cy.wait(500)
+
+        cy.get('input[name="email"]')
+            .type('gmail.com')
+            .should('have.value', 'password@gmail.com')
+
+        cy.contains('this must be a valid email')
+            .should('not.exist')
+
+        cy.wait(500)
     })
+
+    it('Has validation error if PASSWORD is < 6 characters', () => {
+        cy.get('input[name="userPassword"]')
+            .type('ab')
+            .should('have.value', 'ab')
+
+        cy.get('.error')
+            .contains('this must be at least 6 characters')
+
+        cy.wait(500)
+
+        cy.get('input[name="userPassword"]')
+            .type('cd')
+            .should('have.value', 'abcd')
+
+        cy.get('.error')
+            .contains('this must be at least 6 characters')
+
+        cy.wait(500)
+
+        cy.get('input[name="userPassword"]')
+            .type('ef')
+            .should('have.value', 'abcdef')
+
+        cy.contains('this must be at least 6 characters')
+            .should('not.exist')
+
+        cy.wait(500)
+    })
+
+    it('Can check TERMS OF SERVICE, and assertion.', () => {
+        cy.get(`input[name="terms"]`)
+            .check()
+            .should('have.checked')
+    })
+
+    it('Can click SUBMIT, and assertion.', () => {
+        cy.contains('Submit')
+            .click()
+
+        cy.wait(500)
+    })
+})
